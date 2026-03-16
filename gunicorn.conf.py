d8bfae20a68bc6f8cd054e9d2c9ff783525da619
rempairamore <rempairamore@gmail.com>
2026-03-16 16:32:27 +0100

    2.1.2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,14 +5,14 @@ import subprocess
 # Worker configuration
 workers = 5
 worker_class = "gevent"
-worker_connections = 200
+worker_connections = 250
 timeout = 180
 graceful_timeout = 180
 bind = "0.0.0.0:8080"
 
 # Memory limits
 max_requests = 800
-max_requests_jitter = 80
+max_requests_jitter = 200
 
 #The number of seconds to wait for requests on a Keep-Alive connection.
 keepalive = 20
